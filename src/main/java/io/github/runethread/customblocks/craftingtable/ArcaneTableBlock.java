8e1d837996e5563fbf64f1ada18cce597bac02fa
Mysendell <klebinpc@gmail.com>
2025-05-22 10:32:36 -0300

    😭 Rotation works, menus still don't

--- src/main/java/io/github/runethread/customblocks/craftingtable/ArcaneTableBlock.java
+++ src/main/java/io/github/runethread/customblocks/craftingtable/ArcaneTableBlock.java
@@ -1,25 +1,49 @@
 package io.github.runethread.customblocks.craftingtable;
 
+import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
-public class ArcaneTableBlock extends Block implements EntityBlock {
+public class ArcaneTableBlock extends BaseEntityBlock {
+    public static final MapCodec<ArcaneTableBlock> CODEC = simpleCodec(ArcaneTableBlock::new);
+    public static final EnumProperty<Direction> FACING = EnumProperty.create("facing", Direction.class, Direction.Plane.HORIZONTAL);
 
     public ArcaneTableBlock(Properties p_49795_) {
         super(p_49795_);
     }
 
-    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+    @Override
+    protected MapCodec<? extends BaseEntityBlock> codec() {
+        return CODEC;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    protected InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit) {
         System.out.println("ArcaneTableBlock#use called");
         if (!level.isClientSide) {
             BlockEntity be = level.getBlockEntity(pos);
