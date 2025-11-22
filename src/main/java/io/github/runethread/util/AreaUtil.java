c0809ea1ba0210139ac625a06d44257ed6e769ca
Mysendell <kleber.martinsc@gmail.com>
2025-11-22 16:46:14 -0300

    AreaUtil optimization
    Using symmetry in regular shapes we don't need to check every quadrant, only one of them. This significantly reduces the number of iterations for large shapes.

--- src/main/java/io/github/runethread/util/AreaUtil.java
+++ src/main/java/io/github/runethread/util/AreaUtil.java
@@ -31,25 +31,87 @@ public class AreaUtil {
         }
     }
 
+    private static void fourPointSymmetry(BlockPos center, int x, int z, AreaInterface action, ServerLevel level, List<BlockPos> loadedChunkList) {
+        int symX = (2 * center.getX()) - x;
+        int symZ = (2 * center.getZ()) - z;
+        BlockPos pos1 = new BlockPos(x, center.getY(), z);
+        BlockPos pos2 = new BlockPos(x, center.getY(), symZ);
+        BlockPos pos3 = new BlockPos(symX, center.getY(), z);
+        BlockPos pos4 = new BlockPos(symX, center.getY(), symZ);
+        if (ChunkUtils.forceLoadChunk(level, pos1)) loadedChunkList.add(pos1);
+        action.action(pos1);
+        if (ChunkUtils.forceLoadChunk(level, pos2)) loadedChunkList.add(pos2);
+        action.action(pos2);
+        if (ChunkUtils.forceLoadChunk(level, pos3)) loadedChunkList.add(pos3);
+        action.action(pos3);
+        if (ChunkUtils.forceLoadChunk(level, pos4)) loadedChunkList.add(pos4);
+        action.action(pos4);
+    }
+
+    private static void fourPointSymmetry(BlockPos center, int x, int z, int y, AreaInterface action, ServerLevel level, List<BlockPos> loadedChunkList) {
+        int symX = (2 * center.getX()) - x;
+        int symZ = (2 * center.getZ()) - z;
+        BlockPos pos1 = new BlockPos(x, y, z);
+        BlockPos pos2 = new BlockPos(x, y, symZ);
+        BlockPos pos3 = new BlockPos(symX, y, z);
+        BlockPos pos4 = new BlockPos(symX, y, symZ);
+        if (ChunkUtils.forceLoadChunk(level, pos1)) loadedChunkList.add(pos1);
+        action.action(pos1);
+        if (ChunkUtils.forceLoadChunk(level, pos2)) loadedChunkList.add(pos2);
+        action.action(pos2);
+        if (ChunkUtils.forceLoadChunk(level, pos3)) loadedChunkList.add(pos3);
+        action.action(pos3);
+        if (ChunkUtils.forceLoadChunk(level, pos4)) loadedChunkList.add(pos4);
+        action.action(pos4);
+    }
+
+
+    private static void eightPointSymmetry(BlockPos center, int x, int z, int y, AreaInterface action, ServerLevel level, List<BlockPos> loadedChunkList) {
+        int symY = (2 * center.getY()) - y;
+        fourPointSymmetry(center, x, z, y, action, level, loadedChunkList);
+        fourPointSymmetry(center, x, z, symY, action, level, loadedChunkList);
+    }
+
     public static void IterateAreaSphere(ServerLevel level, BlockPos center, double radius, AreaInterface action) {
         Bounding3D bounding = new Bounding3D(center, radius, level.getMinY(), level.getMaxY());
         double radiusSq = radius * radius;
 
         List<BlockPos> loadedChunkList = new ArrayList<>();
 
-        for (int x = bounding.minX(); x <= bounding.maxX(); x++) {
-            for (int y = bounding.minY(); y <= bounding.maxY(); y++) {
-                for (int z = bounding.minZ(); z <= bounding.maxZ(); z++) {
+        for (int x = center.getX(); x <= bounding.maxX(); x++) {
+            for (int y = center.getY(); y <= bounding.maxY(); y++) {
+                for (int z = center.getZ(); z <= bounding.maxZ(); z++) {
                     double dx = x + 0.5 - center.getX();
                     double dy = y + 0.5 - center.getY();
                     double dz = z + 0.5 - center.getZ();
                     if (dx * dx + dy * dy + dz * dz <= radiusSq) {
-                        BlockPos pos = new BlockPos(x, y, z);
-                        if (ChunkUtils.forceLoadChunk(level, pos))
-                            loadedChunkList.add(pos);
-                        action.action(pos);
+                        eightPointSymmetry(center, x, z, y, action, level, loadedChunkList);
                     }
                 }
+            }
+        }
+
+        for (BlockPos pos : loadedChunkList) {
+            ChunkUtils.removeForceLoadChunk(level, pos);
+        }
+    }
+
+    public static void IteratePerimeterCircle(ServerLevel level, BlockPos center, double radius, AreaInterface action) {
+        Bounding2D bounding = new Bounding2D(center, radius);
+        double radiusSq = radius * radius;
+        double innerRadiusSq = (radius - 1) * (radius - 1);
+
+        List<BlockPos> loadedChunkList = new ArrayList<>();
+
+        for (int x = bounding.minX(); x <= center.getX(); x++) {
+            for (int z = bounding.minZ(); z <= center.getZ(); z++) {
+                double dx = x + 0.5 - center.getX();
+                double dz = z + 0.5 - center.getZ();
+
+                double distSq = dx * dx + dz * dz;
+                if (distSq <= radiusSq && distSq >= innerRadiusSq)
+                    fourPointSymmetry(center, x, z, action, level, loadedChunkList);
+
             }
         }
 
@@ -64,16 +126,12 @@ public class AreaUtil {
 
         List<BlockPos> loadedChunkList = new ArrayList<>();
 
-        for (int x = bounding.minX(); x <= bounding.maxX(); x++) {
-            for (int z = bounding.minZ(); z <= bounding.maxZ(); z++) {
+        for (int x = bounding.minX(); x <= center.getX(); x++) {
+            for (int z = bounding.minZ(); z <= center.getZ(); z++) {
                 double dx = x + 0.5 - center.getX();
                 double dz = z + 0.5 - center.getZ();
-                if (dx * dx + dz * dz <= radiusSq) {
-                    BlockPos pos = new BlockPos(x, center.getY(), z);
-                    if (ChunkUtils.forceLoadChunk(level, pos))
-                        loadedChunkList.add(pos);
-                    action.action(pos);
-                }
+                if (dx * dx + dz * dz <= radiusSq)
+                    fourPointSymmetry(center, x, z, action, level, loadedChunkList);
             }
         }
 
@@ -87,13 +145,10 @@ public class AreaUtil {
 
         List<BlockPos> loadedChunkList = new ArrayList<>();
 
-        for (int x = bounding.minX(); x < bounding.maxX(); x++) {
-            for (int y = bounding.minY(); y < bounding.maxY(); y++) {
-                for (int z = bounding.minZ(); z < bounding.maxZ(); z++) {
-                    BlockPos pos = new BlockPos(x, y, z);
-                    if (ChunkUtils.forceLoadChunk(level, pos))
-                        loadedChunkList.add(pos);
-                    action.action(pos);
+        for (int x = center.getX(); x < bounding.maxX(); x++) {
+            for (int y = center.getY(); y < bounding.maxY(); y++) {
+                for (int z = center.getZ(); z < bounding.maxZ(); z++) {
+                    eightPointSymmetry(center, x, z, y, action, level, loadedChunkList);
                 }
             }
         }
